600d9a86b89d911a00d43b2d84299e9aa02ecd43
Kılıç Ali Temiz <katemiz@gmail.com>
2023-04-02 22:14:59 +0300

    End of da commit

--- app/Http/Livewire/Projects.php
+++ app/Http/Livewire/Projects.php
@@ -3,12 +3,21 @@
 namespace App\Http\Livewire;
 
 use App\Models\Project;
-
-
 use Livewire\Component;
 
 class Projects extends Component
 {
+
+    public $action;
+
+    public $projectId;
+    public $projectCode;
+    public $projectTitle;
+    public $projectScope;
+
+
+
+
     public function render()
     {
 
@@ -16,18 +25,24 @@ class Projects extends Component
 
 
         switch ($this->action) {
+
             case 'addproject':
+            default:
+
+                $this->action = 'addproject';
                 $this->projectId = false;
+                $this->projectTitle = false;
+                $this->projectCode = false;
+                $this->projectScope = false;
+
                 break;
 
             case 'viewdoc':
-            //default:
                 $doc = Doc::find($this->docId);
                 $this->setParams($doc,false);
                 break;
         }
 
-        return view('docs.docall');
 
 
 
--- resources/views/livewire/projects.blade.php
+++ resources/views/livewire/projects.blade.php
@@ -1,3 +1,68 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-</div>
+<section class="section container">
+
+
+
+
+
+    @switch($action)
+
+        @case("addproject")
+        @case("editproject")
+
+
+            <div class="column" id="div_project_form">
+
+
+                <header class="my-6">
+                    <h1 class="title has-text-weight-light is-size-1">{{ $projectId ? 'Edit Project' : 'New Project'}}</h1>
+                    <h2 class="subtitle has-text-weight-light">{{ $projectId ? 'Edit Project Attributes' : 'Create New Project'}}</h2>
+                </header>
+
+
+
+                <div class="field">
+                    <label class="label" for="pcode">Project Code</label>
+                    <div class="control" id="pcode">
+                        <input class="input" type="text" wire:model="projectCode" id="projectCode" name="projectCode" value="{{$projectCode}}"  placeholder="Project Code">
+                    </div>
+
+                    @error('projectCode')
+                        <div class="notification is-warning is-light my-2">{{ $message }}</div>
+                    @enderror      
+                </div>
+
+
+                <div class="field">
+                    <label class="label" for="ptitle">Doc Title</label>
+                    <div class="control" id="ptitle">
+                        <input class="input" type="text" wire:model="projectTitle" id="projectTitle" name="projectTitle" value="{{$projectTitle}}"  placeholder="Title of Project">
+                    </div>
+
+                    @error('projectTitle')
+                        <div class="notification is-warning is-light my-2">{{ $message }}</div>
+                    @enderror      
+                </div>
+
+
+                <div class="field" wire:ignore>
+                    <label class="label">Project Scope</label>
+                    <div class="column" wire:model="projectScope" id="projectScope">{!! $projectScope !!}</div>
+                </div>
+
+                <div class="buttons is-right">
+                    <button class="button is-link" onclick="formDocSubmit('{{$action}}')">{{ $projectId ? 'Update' : 'Save New'}}</button>
+                </div>
+
+            </div>
+
+            @break
+
+    @endswitch
+
+
+
+
+
+
+
+</section>
